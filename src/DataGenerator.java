785a11ec0ae0f03d67f16419816654a086d77254
elenam <elenam@morris.umn.edu>
2014-10-01 17:25:04 -0500

    Removed unused imports

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -1,9 +1,6 @@
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.io.Writer;
+
 
 
 public class DataGenerator {
