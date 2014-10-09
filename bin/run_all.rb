9c0fd28a4b6a58c1b21c4a5f8e31d23c4b6c6a46
elenam <elenam@morris.umn.edu>
2014-10-09 12:49:34 -0500

    storing results for each test data set in a separate file

--- bin/run_all.rb
+++ bin/run_all.rb
@@ -1,31 +1,32 @@
 
-### the script for running the 2014 sorting competition  
+### the script for running the 2014 sorting competition 
 
-groups = 15 # number of groups (group 0 is the sample sorting)
+### to pin to one core, run as: taskset -c 0 ruby run_all.rb
+
+groups = 15 # number of groups (including group 0 which is the sample sorting)
 loops = [20, 10, 20]
 runs = 3
 elements = [10000, 50000, 10000]
-lambdas = [1.0, 2.0, 7.0] 
+lambdas = [1.2, 2.8, 7.0] 
 inFileNames = ["in1.txt", "in2.txt", "in3.txt"]
 
 # loop over lambdas and elements
 
 lambdas.length.times do |r| 
   # generate data, store it in a file
+  resultsFile = "results#{r + 1}.txt"
   system("java DataGenerator #{inFileNames[r]} #{elements[r]} #{lambdas[r]}")
-  # some printing here and in results....
-
+  system("java DataGenerator #{inFileNames[r]} #{elements[r]} #{lambdas[r]} >> #{resultsFile}")
+ 
   # run all groups
   groups.times do |j|
     system("echo 'Group#{j}\n'")
-    system("echo '\n Group#{j}: \n' >> results.txt")
+    system("echo '\n Group#{j}: \n' >> #{resultsFile}")
     runs.times do 
-      system("java Group#{j} #{inFileNames[r]} outRun#{r}Group#{j}.txt #{loops[r]} >> results.txt") 
+      system("java Group#{j} #{inFileNames[r]} outRun#{r}Group#{j}.txt #{loops[r]} >> #{resultsFile}") 
     end
-    system("echo 'Results of diff:\n' >> results.txt")
-    system("diff --ignore-all-space --brief outRun#{r}Group#{j}.txt outRun#{r}Group0.txt >> results.txt")
-    # keep the output file this time:     
-    #  system("rm out#{j}.txt")       
+    system("echo 'Results of diff:\n' >> #{resultsFile}")
+    system("diff --ignore-all-space --brief outRun#{r}Group#{j}.txt outRun#{r}Group0.txt >> #{resultsFile}")
   end
 end
 
