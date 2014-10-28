01cd4ba96ba2028bba12d4a3424423286d3e05ab
elenam <elenam@morris.umn.edu>
2014-10-27 22:59:33 -0500

    sorting the resulting arrays by the median

--- bin/run_all.rb
+++ bin/run_all.rb
@@ -23,10 +23,12 @@ def medianOfThree arr
   end
 end
 
+#def sortByMedians 
+
 groups = 15 # number of groups (including group 0 which is the sample sorting)
 loops = [20, 10, 20]
 elements = [10000, 50000, 10000]
-lambdas = [1.2] 
+lambdas = [1.2, 3.2] 
 inFileNames = ["in1.txt", "in2.txt", "in3.txt"]
 runTimes = []
 
@@ -45,13 +47,20 @@ lambdas.length.times do |r|
     system("echo '\n Group#{j}: \n' >> #{resultsFile}")
     runTimes[r][j] = []
     3.times do |k|
-      runTimes[r][j][k] = `java Group#{j} #{inFileNames[r]} outRun#{r + 1}Group#{j}.txt #{loops[r]}` 
+      runTimes[r][j][k] = (`java Group#{j} #{inFileNames[r]} outRun#{r + 1}Group#{j}.txt #{loops[r]}`).to_f
       system("echo '#{runTimes[r][j][k]}' >> #{resultsFile}")
     end
-    system("echo '#{medianOfThree(runTimes[r][j])}' >> #{resultsFile}")
+    system("echo 'Median: #{medianOfThree(runTimes[r][j])}' >> #{resultsFile}")
+    runTimes[r][j][3] = medianOfThree(runTimes[r][j]) #store the median as the last element 
+    runTimes[r][j][4] = j #record the group number
     system("echo 'Results of diff:\n' >> #{resultsFile}")
     system("diff --ignore-all-space --brief outRun#{r + 1}Group#{j}.txt outRun#{r + 1}Group0.txt >> #{resultsFile}")
   end
+  runTimes[r][0][3] = 1000000 # a large number for group 0 (the sample solution)
+  # processing the results
+  system("echo 'This is my array: #{runTimes[r]}'")
+  sortedTimes = (runTimes[r]).sort {|arr1, arr2| arr1[3] <=> arr2[3]}
+  system("echo 'This is sorted array: #{sortedTimes}'")
 end
 
 
